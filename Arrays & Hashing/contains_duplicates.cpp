d10cddaa8febae10292e4a3a210763b6ae0e0b17
piradiusquared <kpan5121@gmail.com>
2026-04-10 12:21:43 +1000

    Contains Duplicates

---
+++ Arrays & Hashing/contains_duplicates.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int len = nums.size();
+        set<int> dict(nums.begin(), nums.end());
+        return (len != dict.size());
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 4, 4};
+    return solution.hasDuplicate(nums);
+}
